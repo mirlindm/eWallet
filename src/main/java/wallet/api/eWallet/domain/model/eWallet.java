e1d2c138a5954415c20fda4816f8e6abe23676ef
Mirlind Murati <mirlindmurati777@gmail.com>
2021-03-19 19:09:27 +0200

    Entities Added

--- src/main/java/wallet/api/eWallet/domain/model/eWallet.java
+++ src/main/java/wallet/api/eWallet/domain/model/eWallet.java
@@ -1,4 +1,63 @@
 package wallet.api.eWallet.domain.model;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="eWALLET")
 public class eWallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="WALLET_ID")
+    private long id;
+
+    @Column(name="ACCOUNT_BALANCE")
+    private BigDecimal balance;
+
+    @OneToMany()
+    private List<Transaction> transactions;
+
+    public eWallet() {
+       balance = new BigDecimal(0);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getbalance() {
+        return balance;
+    }
+
+    public void setbalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public void addTransaction(Transaction tempTransaction) {
+        if (transactions == null) {
+            transactions = new ArrayList<>();
+        }
+
+        transactions.add(tempTransaction);
+
+        tempTransaction.setWallet(this);
+    }
+
+
+
 }
