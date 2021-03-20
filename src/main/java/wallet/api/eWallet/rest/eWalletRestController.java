74c745986aa92f45d316e1f3dd436f7e97d6babb
Mirlind Murati <mirlindmurati777@gmail.com>
2021-03-20 20:04:08 +0200

    Adding endpoints for Transaction

--- src/main/java/wallet/api/eWallet/rest/eWalletRestController.java
+++ src/main/java/wallet/api/eWallet/rest/eWalletRestController.java
@@ -5,7 +5,9 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import wallet.api.eWallet.application.service.TransactionService;
 import wallet.api.eWallet.application.service.eWalletService;
+import wallet.api.eWallet.domain.model.Transaction;
 import wallet.api.eWallet.domain.model.eWallet;
 
 import java.util.List;
@@ -24,6 +26,9 @@ public class eWalletRestController {
 
     @Autowired
     private eWalletService eWalletService;
+
+    @Autowired
+    private TransactionService transactionService;
 
     /**
      * end-point method for getting a eWallet by the ID
@@ -75,6 +80,47 @@ public class eWalletRestController {
 //            return newWallet;
         //}
     }
+
+    /**
+     * end-point method for getting a Transaction by the ID
+     *
+     * @param id - ID of the transaction to be fetched
+     * @return Transaction
+     * @author Mirlind Murati
+     */
+    @GetMapping("/transaction/{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable("id") Long id) throws Exception {
+        Transaction transaction = transactionService.getTransactionById(id);
+
+        return new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+
+    /**
+     * end-point method for fetching all transactions
+     *
+     * @return CollectionModel<Transaction>
+     * @author Mirlind Murati
+     */
+    @GetMapping("/transactions")
+    public ResponseEntity<List<Transaction>> getAllTransactions() throws Exception {
+        List<Transaction> transactions = transactionService.getAllTransactions();
+
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
+    /**
+     * end-point method for creating a new transaction
+     *
+     * @return Transaction
+     * @author Mirlind Murati
+     */
+    @PostMapping("/transactions")
+    public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction) throws Exception {
+        Transaction newTransaction = transactionService.createTransaction(transaction);
+
+        return new ResponseEntity<>(newTransaction, HttpStatus.CREATED);
+    }
+
 
 
 }
